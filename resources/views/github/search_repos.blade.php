c0c10f85d86f9f91290cd296f737b614b5fbaf60
naveenKumarV <naveenkumarvedurupaka@gmail.com>
2015-06-28 22:41:51 +0530

    Updated search_repo.blade.php to add activity graph

--- resources/views/github/search_repos.blade.php
+++ resources/views/github/search_repos.blade.php
@@ -1,7 +1,24 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: naveen kumar v
- * Date: 25/06/2015
- * Time: 14:12
- */
+@extends('template')
+
+@section('heading')
+    SEARCH REPOSITORIES BY KEYWORD
+@stop
+
+@section('body')
+
+    {!! Form::open() !!}
+    <div class="form-group">
+        {!! Form::label('repo','Enter the Repository Name') !!}
+        {!! Form::text('repo',null,['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::submit('Display Repositories',['class'=>'btn btn-primary form-control']) !!}
+    </div>
+    {!! Form::close() !!}
+
+    @if(isset($lava))
+        <div id="pop_div"></div>
+        <?php echo $lava->render('AreaChart', 'Activity', 'pop_div') ?>
+    @endif
+
+@stop
